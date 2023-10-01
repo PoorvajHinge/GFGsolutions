e53c406e1a7cb96feac8c7d3446a5e20215b5192
poorvajhinge <74683290+PoorvajHinge@users.noreply.github.com>
2023-10-01 15:33:27 +0530

    Added Solution - GfG to GitHub

---
+++ School/Palindromic Array/palindromic-array.py
@@ -0,0 +1,26 @@
+# Your task is to complete this function
+# Function should return True/False or 1/0
+def PalinArray(arr ,n):
+    # Code here
+    for i in range(0,len(arr)):
+        x=str(arr[i])
+        if str(arr[i]) != x[::-1]:
+            return 0
+    return 1
+
+
+#{ 
+ # Driver Code Starts
+# Driver Program
+if __name__=='__main__':
+    t=int(input())
+    for i in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        if PalinArray(arr, n):
+            print(1)
+        else:
+            print(0)
+# Contributed By: Harshit Sidhwa
+
+# } Driver Code Ends
