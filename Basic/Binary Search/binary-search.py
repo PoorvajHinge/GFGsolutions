abb94702f3c7618c58f0e01cc3b5f558658e9596
PoorvajHinge <74683290+PoorvajHinge@users.noreply.github.com>
2023-12-31 16:53:35 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Binary Search/binary-search.py
@@ -0,0 +1,33 @@
+#User function template for Python
+
+class Solution:	
+	def binarysearch(self, arr, n, k):
+		# code here
+		low = 0
+		high = n-1
+		while low <=high:
+		    mid = (high+low)//2
+		    if k==arr[mid]:
+		        return mid
+		    elif(k>arr[mid]):
+		        low = mid + 1 
+		    else:
+		        high = mid -1 
+		return -1 
+
+
+#{ 
+ # Driver Code Starts
+#Initial template for Python
+
+if __name__ == '__main__':
+    t=int(input())
+    for i in range(t):
+        n=int(input())
+        arr=list(map(int, input().strip().split(' ')))
+        k=int(input())
+        ob = Solution()
+        print (ob.binarysearch(arr, n, k))
+
+
+# } Driver Code Ends
