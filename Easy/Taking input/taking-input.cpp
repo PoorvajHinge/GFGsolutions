cdcb064f7759ffa7b0c74bb9bf6e826be92fff36
PoorvajHinge <74683290+PoorvajHinge@users.noreply.github.com>
2024-01-19 11:19:04 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Taking input/taking-input.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<iostream>
+using namespace std;
+
+ void inputData();
+ 
+
+// } Driver Code Ends
+//User function Template for C++
+
+ void inputData()
+{
+       int a;
+       string b;
+       //Use cin to take input and cout a and b with a space between them.
+       cin>>a>>b;
+       //You don't have to print any new lines after cout 
+       cout<<a<<" "<<b;
+}
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //function call
+        inputData();
+        cout<<endl;
+    }
+}
+
+
+
+// } Driver Code Ends
