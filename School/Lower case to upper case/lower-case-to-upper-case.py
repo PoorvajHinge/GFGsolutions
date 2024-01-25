9be3d77bdde145dfd8a51a6623a99cb4e27293b8
PoorvajHinge <74683290+PoorvajHinge@users.noreply.github.com>
2024-01-26 00:32:31 +0530

    Added Solution - GfG to GitHub

---
+++ School/Lower case to upper case/lower-case-to-upper-case.py
@@ -0,0 +1,18 @@
+#User function Template for python3
+class Solution:
+    def to_upper(self, str):
+        # code here
+        return str.upper()
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        str = input().strip()
+        ob = Solution()
+        print(ob.to_upper(str))
+# } Driver Code Ends
