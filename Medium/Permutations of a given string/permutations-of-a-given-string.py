a8dd4d57164a00d25edd5d95b9a7786458a6eb78
PoorvajHinge <74683290+PoorvajHinge@users.noreply.github.com>
2024-01-30 07:50:27 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Permutations of a given string/permutations-of-a-given-string.py
@@ -0,0 +1,27 @@
+#User function Template for python3
+from itertools import permutations
+class Solution:
+    def find_permutation(self, S):
+        # Code here
+        perms = [''.join(x) for x in permutations(S)]
+        return sorted(set(perms))
+   
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+if __name__ == '__main__':
+	t=int(input())
+	for i in range(t):
+		S=input()
+		ob = Solution()
+		ans = ob.find_permutation(S)
+		ans.sort()
+		for i in ans:
+			print(i,end=" ")
+		print()
+# } Driver Code Ends
