4c1e882025e1697b5179f58816d769e570c72930
poorvajhinge <74683290+PoorvajHinge@users.noreply.github.com>
2025-06-08 15:27:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Array insert at index/array-insert-at-index.py
@@ -0,0 +1,11 @@
+class Solution:
+    '''You need to insert the given element at the given index. 
+    After inserting the elements at index, elements
+    from index onward should be shifted one position ahead
+     You may assume that the array already has sizeOfArray - 1
+    elements.'''
+    
+    def insertAtIndex(self, arr, sizeOfArray, index, element):
+        #Your code here
+        arr.insert(index,element)
+        return arr
